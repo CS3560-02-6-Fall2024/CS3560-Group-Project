9a9205366b195c58dd631818ad1f74c1f5073b3f
Kikie <152471448+kikieliu@users.noreply.github.com>
2024-11-01 19:41:35 -0700

    Update Supplier.java

--- Supplier.java
+++ Supplier.java
@@ -1,15 +1,72 @@
+import.java.util.*;
+
 public class Supplier
 {
+	private static int idCounter = 1; // Counter to ensure unique supplier IDs
+	private static List<Supplier> suppliers = new ArrayList<>(); // List of all suppliers
+	
+	private int supplierID; // Primary key for suppliers
 	private String name; // Name of the supplier
-	private String phoneNumber; // Number of the supplier (Contact person?)
-	private String email; // Email of the supplier (Contact person?)
-	// TODO: INGREDIENTS SOLD FIELD
+	private String phoneNumber; // Number of the supplier 
+	private String email; // Email of the supplier
+	private String description; // Information user wants to note down, such as ingredients sold 
 
 	// Constructor for Supplier
-	public Supplier(String _name, String _phoneNumber, String _email)
+	public Supplier(String _name, String _phoneNumber, String _email, String _description)
 	{
-		name = _name;
-		phtonNumber = _phoneNumber;
-		email = _email;
+		this.supplierID = idCounter++;
+		this.name = _name;
+		this.phoneNumber = _phoneNumber;
+		this.email = _email;
+		this.description = _description;
+		suppliers.add(this);
 	}
+
+	// Individual Update Methods 
+	public void updateName(String newName) 
+	{
+		this.name = newName;
+	}
+
+	public void updateNumber(String newNumber) 
+	{
+		this.phoneNumber = newNumber;
+	}
+
+	public void updateEmail(String newEmail) 
+	{
+		this.email = newEmail;
+	}
+	
+	public void updateDescription(String newDescription)
+	{
+		this.description = newDescription;
+	}
+
+	// Method to Remove Supplier
+	public boolean removeSupplier(String name)
+	{
+		// For loop that loops through supplier list 
+		// If-else loop to match by name 
+		// Return true if successfully removed, false if not
+	}
+
+	// Method to Search for Suppler (by name)
+	public static Supplier searchSupplier(String name) 
+	{
+		// For loop that loops through supplier list
+		// If-else loop to match by name
+		// If found, return supplier, if not, return null
+	}
+
+	// Method to Display Supplier Information
+	public void displaySupplierInfo()
+	{
+		// Print headings for each attribute on a new line
+		// Ex: "Name: "
+	}
+
+	// Getters
+	
+		
 }
