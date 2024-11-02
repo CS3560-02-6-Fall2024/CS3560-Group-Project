66b3c5fd04a78ba97a4b201e11fcff86d9336006
Plutzz <benjiongco@gmail.com>
2024-11-01 19:05:32 -0700

    Split updateBatch into addToBatch and removeFromBatch

--- Batch.java
+++ Batch.java
@@ -5,13 +5,19 @@ public class Batch
 	private String expirationDate; // Date of expiration of ALL products in this batch (MM/DD/YYYY)
 
 
-	// Updates this batch's quantity after using "amount" number of items
-	private void updateBatch(int amount)
+	// Removes from this batch's quantity after using "amount" number of items
+	private void removeFromBatch(int amount)
 	{
 		// if quantity - amount < 0, we don't have enough products in this batch, search for other batches with this item
 		// quantity = quantity - amount
 		// if quantity = 0, remove/ notify to remove batch
 	}
 
+	// Adds to this batch's quantity after using "amount" number of items
+	// Used to fix batch information when a batch is incorrectly entered into the system
+	private void addToBatch(int amount)
+	{
+		// quantity = quantity + amount
+	}
 
 }
