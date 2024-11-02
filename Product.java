d5f2b214af12f6351e27ce06594092e9c796a0cf
Plutzz <benjiongco@gmail.com>
2024-11-01 20:10:37 -0700

    Add product use cases

--- Product.java
+++ Product.java
@@ -1,6 +1,31 @@
 public class Product
 {
+	private int productID;
 	private String name; // The display name of the product
 	private String description; // Short description of the product
 	private int totalQuantity; // Amount of this product in the order?
+
+
+	// Constructor for Product
+	public Product(int _productID, String _name, String _desc)
+	{
+		productID = _productID;
+		name = _name;
+		description = _desc;
+	}
+
+	// Updates product name and description
+	public void updateProduct(String _name, String _desc)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	// Removes this product and deletes all related batches from system
+	// Returns true if deletion is sucessful, false otherwise
+	public boolean deleteProduct()
+	{
+		//destroy this
+		//destroy related batches
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 }
