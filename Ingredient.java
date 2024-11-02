eebf14d30d5599052ffbcde67264b6cda52b18dd
Kikie <152471448+kikieliu@users.noreply.github.com>
2024-11-01 18:50:30 -0700

    Update Ingredient.java

--- Ingredient.java
+++ Ingredient.java
@@ -2,5 +2,5 @@ public class Ingredient
 {
 	private String name; // Name of the ingredient
 	private String description; // Short description of the ingredient
-	private int totalQuantity; // Amount of this ingredient that we have (Units?)
+	private int totalQuantity; // Amount of this ingredient that we have (All batches combined)
 }
