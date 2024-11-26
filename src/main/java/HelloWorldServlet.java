601ae7cc709b409818b21a8dde5c11f260750756
Plutzz <benjiongco@gmail.com>
2024-11-25 16:11:07 -0800

    Tomcat test

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -4,6 +4,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.*;
 
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
@@ -12,6 +13,7 @@ public class HelloWorldServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>TEST</h1>");
   }
 }
+
