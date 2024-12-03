b4177b912770b71319c233e36de79aff17015440
Anson <146302314+Weplayt@users.noreply.github.com>
2024-12-02 20:49:39 -0800

    Had To Fix name
    Servlet

--- src/main/java/SupplierServlet.java
+++ src/main/java/SupplierServlet.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class SupplierServerlet extends HttpServlet {
+public class SupplierServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
