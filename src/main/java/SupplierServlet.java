ccccad649a5232aac59d5f43a00bcffa3ba04b2d
Anson <146302314+Weplayt@users.noreply.github.com>
2024-12-02 21:22:31 -0800

    SupplierServlet html copy
    I will try to learn the html code stuff tomorrow

--- src/main/java/SupplierServlet.java
+++ src/main/java/SupplierServlet.java
@@ -1,21 +1,61 @@
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+//when suppliers.html is accessed
+@WebServlet("/suppliers.html")
 public class SupplierServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
     {
 
-        //get parameters
-        String itemID = request.getParameter("itemID");
-        boolean product = itemID.substring(0,1).equals("P");
-        int id = Integer.parseInt(itemID.substring(1));
+        // Render html page (just copies the html of the page that you are using)
+        File html = new File(System.getProperty("user.dir") + "/src/main/webapp/suppliers.html");
+        Scanner scan = new Scanner(html, "UTF-8");
+        response.setContentType("text/html;");
+        PrintWriter out = response.getWriter();
 
+        while(scan.hasNextLine())
+        {
+            String nextLine = scan.nextLine();
 
+            // Copy the result box html so we can to populate page
+            if(nextLine.contains("START RESULT BOX"))
+            {
+                // Skip past this result box section
+                while(scan.hasNextLine())
+                {
+                    nextLine = scan.nextLine();
+                    // Break out of result box loop if we reach the end of the result box
+                    if(nextLine.contains("END RESULT BOX"))
+                        break;
+                }
+                
+                ArrayList<Supplier> suppliers = DatabaseGetter.getSuppliers();
+                // Add results box n times into the page
+                for(Supplier supplier : suppliers)
+                {
+                    out.println(supplier); //I'm not sure how to set up html boxes so for now it'll be this
+                }
+            }
+            // Copy html if it is not part of results box
+            else
+            {
+                out.println(nextLine);
+            }
+        }
+
+        scan.close();
+
+        
     }
 
 
