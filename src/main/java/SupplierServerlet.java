bd29deb2f472a97269d0ded81e5d41e63752eb3b
Anson <146302314+Weplayt@users.noreply.github.com>
2024-12-02 17:43:47 -0800

    DatabaseGetter getSuppliers() method
    Added getSuppliers to the DatabaseGetter class, started on SupplierServlet

---
+++ src/main/java/SupplierServerlet.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SupplierServerlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+    {
+
+        //get parameters
+        String itemID = request.getParameter("itemID");
+        boolean product = itemID.substring(0,1).equals("P");
+        int id = Integer.parseInt(itemID.substring(1));
+
+
+    }
+
+
+
+
+}
