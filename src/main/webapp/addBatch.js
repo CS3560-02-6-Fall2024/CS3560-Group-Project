a27cde28c31ce18c2961f87e5c6b689c435b6d43
KALE <114507504+kaleyvuong@users.noreply.github.com>
2024-12-05 02:42:51 -0800

    Fixed batch fields formatting

--- src/main/webapp/addBatch.js
+++ src/main/webapp/addBatch.js
@@ -43,59 +43,59 @@ function App() {
                 <div class="box">
                     <div class="batchID">#{index + 1}</div>
                     <div className="top-row">
-                      <div className="input-row">
-                        {/* Quantity Field */}
-                        <div className="input-container">
-                          <div className="text">Quantity</div>
-                          <input
-                            type="text"
-                            name="quantity"
-                            placeholder="Enter here..."
-                            value={form.quantity}
-                            onChange={(event) => handleFormChange(event, index)}
-                          />
-                        </div>
+                      {/* Quantity Field */}
+                      <div className="input-container">
+                        <div className="text">Quantity</div>
+                        <input
+                          type="text"
+                          name="quantity"
+                          placeholder="Enter here..."
+                          value={form.quantity}
+                          onChange={(event) => handleFormChange(event, index)}
+                        />
+                      </div>
 
-                        {/* Units Field */}
-                        <div className="input-container">
-                          <div className="text">Units</div>
-                          <input
-                            type="text"
-                            name="unit"
-                            placeholder="Enter here..."
-                            value={form.unit}
-                            onChange={(event) => handleFormChange(event, index)}
-                          />
-                        </div>
+                      {/* Units Field */}
+                      <div className="input-container">
+                        <div className="text">Units</div>
+                        <input
+                          type="text"
+                          name="unit"
+                          placeholder="Enter here..."
+                          value={form.unit}
+                          onChange={(event) => handleFormChange(event, index)}
+                        />
                       </div>
                     </div>
-                    <div class="text">Expiration Date</div>
-                    <input type="text"
-                    name="expirationDate"
-                    placeholder='Enter here...'
-                    value={form.expirationDate}
-                    onChange={event => handleFormChange(event, index)}
-                    />
+                    <div class="bottom">
+                      <div class="text">Expiration Date</div>
+                      <input type="text"
+                      name="expirationDate"
+                      placeholder='Enter here...'
+                      value={form.expirationDate}
+                      onChange={event => handleFormChange(event, index)}
+                      />
 
-                    <div class="text">Creation Date</div>
-                    <input type="text"
-                    name="creationDate"
-                    placeholder='Enter here...'
-                    value={form.creationDate}
-                    onChange={event => handleFormChange(event, index)}
-                    />
+                      <div class="text">Creation Date</div>
+                      <input type="text"
+                      name="creationDate"
+                      placeholder='Enter here...'
+                      value={form.creationDate}
+                      onChange={event => handleFormChange(event, index)}
+                      />
 
-                    <div class="text">Order Status</div>
-                    <input type="text"
-                    name="orderStatus"
-                    placeholder='Enter here...'
-                    value={form.orderStatus}
-                    onChange={event => handleFormChange(event, index)}
-                    />
+                      <div class="text">Order Status</div>
+                      <input type="text"
+                      name="orderStatus"
+                      placeholder='Enter here...'
+                      value={form.orderStatus}
+                      onChange={event => handleFormChange(event, index)}
+                      /></div>
+
                     <div class="buttons">
                       <button class="save" onClick={() => removeFields(index)}> Save </button>
                       <button class="remove" onClick={() => removeFields(index)}> Remove </button>
-                      </div>
+                    </div>
                     <div class="batch-number">Batch Number: {index + 1}</div>
                 </div>
               </div>
